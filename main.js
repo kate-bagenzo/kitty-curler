ec2918d86c2ab559ab1b4ae322c12ed893781336
Kate Bagenzo <katebagenzo@gmail.com>
2023-10-04 19:36:55 +0200

    fix yaml

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ import './style.css'
 var slider = document.getElementById('slider');
 var gato = document.getElementById('gato');
 
+function preload(url) {
+  let img = new Image();
+  img.src = url;
+}
+
+for (let i = 0; i <= 60; i++) {
+  preload("img/gato" + i + ".jpg");
+}
+
 slider.addEventListener('input', () => {
   gato.src = "img/gato" + slider.value + ".jpg";
 })
